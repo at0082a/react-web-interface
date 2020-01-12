8162b71a8ed1b98c97fa2d6e4294f6cde0f96eaf
Alex Tilatti <alex.tilatti@gmail.com>
2020-01-12 02:46:37 -0500

    edited styling and project setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className="page bg-blue" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-blue">
+              <div className="container">
+                <div>Add Appointments</div>
+                <div>Search Appointments</div>
+                <div>List Appointments</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+  );
+}
+
+export default App;
