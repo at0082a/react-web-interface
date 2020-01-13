aafd5233678b029d28f106319bb082375718fa70
Alex Tilatti <alex.tilatti@gmail.com>
2020-01-13 00:40:09 -0500

    added a delete appointment event handler

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     this.state = {
       myAppointments : []
     };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
   }
 
   componentDidMount () {
@@ -19,24 +20,31 @@ class App extends Component {
       this.setState({myAppointments: items});
     });
   }
+
+  deleteAppointment(apt) {
+    console.log(apt);
+    let appts = this.state.myAppointments;
+    let updatedAppts = appts.filter((item, index) => index !== apt);
+    this.setState({myAppointments: updatedAppts});
+  }
  
   render () {
     let appointments = this.state.myAppointments;
 
     return (
       <main className="page bg-white" id="petratings">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 bg-white">
-                <div className="container">
-                  <AddAppointment/>
-                  <SearchAppointment/>
-                  <ListAppointment appointments={appointments}/>
-                </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointment/>
+                <SearchAppointment/>
+                <ListAppointment appointments={appointments} deleteAppointment={this.deleteAppointment}/>
               </div>
             </div>
           </div>
-        </main>
+        </div>
+      </main>
     );
   }
 }
--- src/components/listAppointment.js
+++ src/components/listAppointment.js
@@ -3,15 +3,22 @@ import { FaTimes } from 'react-icons/fa';
 import Moment from 'react-moment';
 
 export default class ListAppointment extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <div className="appointment-list item-list mb-3">
         
       {this.props.appointments.map((item, i) => {
           return (
-        <div key={i} className="pet-item col media py-3">
+        <div key={i} onClick={(e) => {this.props.deleteAppointment(i)}} className="pet-item col media py-3">
           <div className="mr-3">
-              <button className="pet-delete btn btn-sm btn-danger">
+              <button className="pet-delete btn btn-sm btn-danger" >
                 <FaTimes/>
               </button>
           </div>
@@ -20,10 +27,9 @@ export default class ListAppointment extends Component {
               <div className="pet-head d-flex">
                 <span className="pet-name">{item.petName}</span>
                 <span className="apt-date ml-auto">
-                  <Moment 
-                  date={item.aptDate}
-                  parse="YYYY-MM-dd hh:mm"
-                  />
+                  <Moment parse="YYYY/MM/DD h:mma">
+                    {item.aptDate}
+                  </Moment> 
                 </span>
               </div>
 
